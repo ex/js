47c180b36bcb2a2eef9cd649ace03e26e33ba9dd
ex <exeqtor@gmail.com>
2024-04-26 14:01:03 -0500

    updated for vscode

--- src/stc/main.js
+++ src/stc/main.js
@@ -28,6 +28,34 @@
 /*   OTHER DEALINGS IN THE SOFTWARE.                                          */
 /* -------------------------------------------------------------------------- */
 var Stc;
+/* ========================================================================== */
+/*                          STC - SIMPLE TETRIS CLONE                         */
+/* -------------------------------------------------------------------------- */
+/*   A simple tetris clone.                                                   */
+/*                                                                            */
+/*   Copyright (c) 2015 Laurens Rodriguez Oscanoa.                            */
+/*                                                                            */
+/*   Permission is hereby granted, free of charge, to any person              */
+/*   obtaining a copy of this software and associated documentation           */
+/*   files (the "Software"), to deal in the Software without                  */
+/*   restriction, including without limitation the rights to use,             */
+/*   copy, modify, merge, publish, distribute, sublicense, and/or sell        */
+/*   copies of the Software, and to permit persons to whom the                */
+/*   Software is furnished to do so, subject to the following                 */
+/*   conditions:                                                              */
+/*                                                                            */
+/*   The above copyright notice and this permission notice shall be           */
+/*   included in all copies or substantial portions of the Software.          */
+/*                                                                            */
+/*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,          */
+/*   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES          */
+/*   OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND                 */
+/*   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT              */
+/*   HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,             */
+/*   WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING             */
+/*   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR            */
+/*   OTHER DEALINGS IN THE SOFTWARE.                                          */
+/* -------------------------------------------------------------------------- */
 (function (Stc) {
     // Data structure that holds information about our tetromino blocks.
     var StcTetromino = /** @class */ (function () {
@@ -1095,4 +1123,4 @@ window.onload = function () {
         setInterval(update, 1000 / Stc.PlatformHTML5.FPS);
     };
 };
-//# sourceMappingURL=platform.js.map
+//# sourceMappingURL=main.js.map
